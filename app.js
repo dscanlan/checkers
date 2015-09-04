4fa639decc265527a3535b007f8fe524bfdb3d05
Dominic Scanlan <dominicscanlan@hotmail.com>
2015-09-04 21:18:10 +0100

    added Create game

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var app = express();
 var config = require('./checkers_config');
 
 var http = require('http').Server(app);
-
-var socketio = require('./checkers_socket');
+var socketio = require('socket.io')(http);
+//var socketio = require('./checkers_socket'); // this isn't working. Perhaps its becuase it uses a new http and listen is being set here.
 var redis = require('./checkers_redis');
 
 
@@ -19,6 +19,17 @@ app.get('/', function(req, res){
 
 
 
+socketio.on('connection', function(socket){
+	console.log('user connected');
+	socket.on('disconnected', function(){
+		console.log('user disconnected');
+	});
+	socket.on('chat message', function(message){
+		//socket.broadcast.emit(message);
+		socket.emit('chat message',message);
+		console.log('message: ' + message);
+	});
+});
 
 
 http.listen(config.server_port, function(){
--- checkers_redis.js
+++ checkers_redis.js
@@ -1,11 +1,22 @@
 var redis = require('redis');
 var config = require('./checkers_config');
 var client = redis.createClient(); //By default, redis.createClient() will use 127.0.0.1 and 6379 as the hostname and port 
-
+var uuid = require('node-uuid');
 client.on('connect', function(){
 	console.log('connected to redis');
 })
 
+checkAwaitingGames = function(){
+	
+}
 
+createGame = function(sessionid){
+	var uuid1 = uuid.v4();
+	var game = config.redis_root_key+uuid1;
+	client.hmset(game, {
+		'player1': sessionid,
+		'player2': ''
+	});
+}
 
 module.exports = client;
